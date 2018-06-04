f756cb077085ea4f6e4178a6c7671226f79eff53
Johan Quiroga <johan.c.quiroga@gmail.com>
2018-06-04 17:17:39 -0500

    Added login form page

---
+++ src/components/account/LoginPage.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="row justify-content-center">
+        <div className="col-10 col-sm-7 col-md-5 col-lg-4">
+          <Form>
+            <FormGroup>
+              <Label htmlFor="exampleEmail">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="contact@johanquiroga.me"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor="examplePassword">Email</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="password"
+              />
+            </FormGroup>
+            <Button>Log In</Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
