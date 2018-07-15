5d443de7a8bc90ab5be77d33ad168dfbfa03df3d
Johan Quiroga <johan.c.quiroga@gmail.com>
2018-07-15 18:09:04 -0500

    Added front-end validation

--- src/components/account/RegisterPage.jsx
+++ src/components/account/RegisterPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { AvForm, AvGroup, AvInput, AvFeedback } from 'availity-reactstrap-validation';
+import { Button, Label } from 'reactstrap';
 
 class RegisterPage extends React.Component {
   constructor(props) {
     super(props);
 
     // bound functions
-    this.compileFormData = this.compileFormData.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleValidSubmit = this.handleValidSubmit.bind(this);
 
     // component state
     this.state = {
@@ -31,7 +32,7 @@ class RegisterPage extends React.Component {
     }
   }
 
-  compileFormData() {
+  handleValidSubmit() {
     const { registerFunction } = this.props;
     const formData = this.state;
     registerFunction(formData);
@@ -43,81 +44,92 @@ class RegisterPage extends React.Component {
         <div className="col-10 col-sm-7 col-md-5 col-lg-4">
           <p>
             Want to het started saving ypur favorite bands to MusicList?
-            Create an account!
+            Create an account! All fields are required
           </p>
-          <Form>
-            <FormGroup>
+          <AvForm onValidSubmit={this.handleValidSubmit}>
+            <AvGroup>
               <Label for="email">Email</Label>
-              <Input
+              <AvInput
                 id="email"
                 name="email"
                 type="email"
                 placeholder="noreply@musiclist.com"
                 value={this.state.email}
+                required
                 onChange={this.handleInputChange}
                 onKeyPress={this.handleKeyPress}
               />
-            </FormGroup>
+              <AvFeedback>A valid email is required to register</AvFeedback>
+            </AvGroup>
 
-            <FormGroup>
+            <AvGroup>
               <Label for="password">Password</Label>
-              <Input
+              <AvInput
                 id="password"
                 name="password"
                 type="password"
                 placeholder="password"
                 value={this.state.password}
+                required
+                minLength="8"
                 onChange={this.handleInputChange}
                 onKeyPress={this.handleKeyPress}
               />
+              <AvFeedback>Passwords must be at least eight characters in length</AvFeedback>
               <span>
                 we recommend a password service like&nbsp;
                 <a href="https://www.lastpass.com/" target="_blank" rel="noopener noreferrer">LastPass</a>
                 &nbsp;or <a href="https://1password.com/" target="_blank" rel="noopener noreferrer">1Password</a>
               </span>
-            </FormGroup>
+            </AvGroup>
 
-            <FormGroup>
+            <AvGroup>
               <Label for="username">Username</Label>
-              <Input
+              <AvInput
                 id="username"
                 name="username"
                 type="text"
                 placeholder="CaptainCode"
                 value={this.state.username}
+                required
                 onChange={this.handleInputChange}
                 onKeyPress={this.handleKeyPress}
               />
-            </FormGroup>
+              <AvFeedback>A username is required to register</AvFeedback>
+            </AvGroup>
 
-            <FormGroup>
+            <AvGroup>
               <Label for="firstName">First Name</Label>
-              <Input
+              <AvInput
                 id="firstName"
                 name="firstName"
                 type="text"
                 placeholder="John"
                 value={this.state.firstName}
+                required
                 onChange={this.handleInputChange}
                 onKeyPress={this.handleKeyPress}
               />
-            </FormGroup>
+              <AvFeedback>A first name is required to register</AvFeedback>
+            </AvGroup>
 
-            <FormGroup>
+            <AvGroup>
               <Label for="lastName">Last Name</Label>
-              <Input
+              <AvInput
                 id="lastName"
                 name="lastName"
                 type="text"
                 placeholder="Smith"
                 value={this.state.lastName}
+                required
                 onChange={this.handleInputChange}
                 onKeyPress={this.handleKeyPress}
               />
-            </FormGroup>
+              <AvFeedback>A last name is required to register</AvFeedback>
+            </AvGroup>
 
-            <Button color="primary" onClick={this.compileFormData}>Register</Button>
-          </Form>
+            <Button color="primary">Register</Button>
+          </AvForm>
         </div>
       </div>
     );
