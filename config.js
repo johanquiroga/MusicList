3ae3b866530a0cd179b1a03818eb14c110ccb57b
Johan Quiroga <johan.c.quiroga@gmail.com>
2018-07-29 18:31:56 -0500

    Added discog client information and added albums models

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const configPath = './config.json';
 const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 
 exports.crypto = parsed.crypto;
+exports.discogs = parsed.discogs;
 exports.expressSession = parsed.expressSession;
 exports.mailgun = parsed.mailgun;
 exports.port = parsed.port;
